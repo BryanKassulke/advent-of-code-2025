14246de694220e3804479e67ad9492b71e11d8e4
Bryan Kassulke <bryan.kassulke@axcelerate.com>
2025-12-05 00:30:33 +0000

    day-01 part two

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bry Bry's Advent of Code 2025
+
+- Maybe a language other than JavaScript?
+- Check out the better solutions from my pal [Alec](https://github.com/alecbass/advent-of-code-2025) :DDD
+
+![test](https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHl3YjIzc2hrZmd0a3hseGZ1ZzV3dnowZDZzemR3ajIyYWZ1Mno2bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/e7CD41ekE5ZUSgTnkn/giphy.gif)
--- day-01.js
+++ day-01.js
@@ -12,14 +12,38 @@ fs.readFile("./day-01-input.txt", "utf8", (error, data) => {
     return;
   }
   const result = data.split("\n").reduce(
-    (state, line) => {
-      const direction = line[0] === "R" ? 1 : -1;
-      const steps = parseInt(line.slice(1), 10);
-      const index = (state.index + direction * steps + 100) % 100;
-      if (index === 0) {
-        state.count += 1;
+    ({ index, count }, line) => {
+      const direction = line[0];
+      let steps = parseInt(line.slice(1), 10);
+      if (steps >= 100) {
+        if (PART === 2) {
+          count += Math.floor(steps / 100);
+        }
+        steps = steps % 100;
       }
-      return { ...state, index };
+      let newIndex;
+      if (direction === "R") {
+        newIndex = index + steps;
+        if (newIndex >= 100) {
+          if (PART === 2 && newIndex != 100) {
+            count++;
+          }
+          newIndex = newIndex % 100;
+        }
+      } else if (direction === "L") {
+        newIndex = index - steps;
+        if (newIndex < 0) {
+          if (PART === 2 && index > 0) {
+            count++;
+          }
+          newIndex = newIndex + 100;
+        }
+      }
+      if (newIndex === 0) {
+        count++;
+      }
+      index = newIndex;
+      return { index, count };
     },
     { index: 50, count: 0 }
   );
@@ -27,3 +51,13 @@ fs.readFile("./day-01-input.txt", "utf8", (error, data) => {
 });
 
 // Result for my input: 1071
+
+/**
+ * --- Part Two ---
+ * We now also have to count the number of times we end up on 0 OR turn past it
+ * (including multiple turns in a single instruction). This was was pretty tedious
+ * to troubleshoot and it was challenging pinpoint certain edge cases. If in doubt
+ * reduce the by parsing/simplifying the input as much as possible.
+ */
+
+const PART = 2; // 1 or 2
